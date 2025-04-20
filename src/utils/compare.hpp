655903a016422a5412bed83e066d415d86fa49ed
Vinh1222001 <nguyentuanvinh1222@gmail.com>
2025-04-20 12:23:00 +0700

    Print out wrong constraint error

--- src/utils/compare.hpp
+++ src/utils/compare.hpp
@@ -9,6 +9,11 @@ namespace CompareUtils
   template <typename T>
   bool isInConstraint(T value, T from, T to)
   {
+    if (to < from)
+    {
+      ESP_LOGE("COMPARE_UTILS.IS_IN_CONSTRAINT", "WRONG Constraint!");
+      return false;
+    }
     return value >= from && value <= to;
   }
 }
