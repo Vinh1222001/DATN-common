2b3902aa073f1d7223fd71d8ac500677d44cb3a3
Vinh1222001 <nguyentuanvinh1222@gmail.com>
2025-04-20 12:06:21 +0700

    Add compare Utils

---
+++ src/utils/compare.hpp
@@ -0,0 +1,14 @@
+#pragma once
+#ifndef COMPARE_UTILS_HPP
+#define COMPARE_UTILS_HPP
+
+#include <Arduino.h>
+
+namespace CompareUtils
+{
+  template <typename T>
+  bool isInConstraint(T value, T from, T to)
+  {
+    return value >= from && T <= to;
+  }
+}
